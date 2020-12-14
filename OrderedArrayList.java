1d4e155fad935bc91e9e51ab3d776aebdd2566b5
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-12-13 20:40:40 -0500

    add2, set

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,12 +1,29 @@
-OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
 
+  OrderedArrayList(){
+    super();
+  }
 
+  OrderedArrayList(int index){
+    super(index);
+  }
 
-public void add(int index, T value) throws IllegalArgumentException() {
-  if (value == null) throw new IllegalArgumentException();
-  super.add(index, value);
-}
+  public boolean add(T value) throws IllegalArgumentException {
+    if (value == null) throw new IllegalArgumentException();
+    return super.add(value);
+  }
 
+  public void add(int index, T value) throws IllegalArgumentException {
+    if (value == null) throw new IllegalArgumentException();
+    super.add(index, value);
+  }
+
+  public T set(int index, T value) throws IllegalArgumentException {
+    if (value == null) throw new IllegalArgumentException();
+    T ans = super.remove(index);
+    super.add(index, value);
+    return (ans);
+  }
 
 
 
