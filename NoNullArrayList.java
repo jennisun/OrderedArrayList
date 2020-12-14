4b0f1540fad30a84ac644673a84c812edb17abf0
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2020-12-13 20:28:23 -0500

    added throw stuff

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -11,17 +11,17 @@ class NoNullArrayList<T> extends ArrayList<T> {
   }
 
 
-  public boolean add(T value){
+  public boolean add(T value) throws IllegalArgumentException {
     if (value == null) throw new IllegalArgumentException();
     return super.add(value);
   }
 
-  public void add(int index, T value){
+  public void add(int index, T value) throws IllegalArgumentException {
     if (value == null) throw new IllegalArgumentException();
     super.add(index, value);
   }
 
-  public T set(int index, T value) {
+  public T set(int index, T value) throws IllegalArgumentException {
     if (value == null) throw new IllegalArgumentException();
     return super.set(index, value);
   }
